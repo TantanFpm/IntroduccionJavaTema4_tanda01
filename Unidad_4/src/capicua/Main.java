57e02228b88d3c5ec599718eec6a533ff01a9965
TantanFpm <48942110+TantanFpm@users.noreply.github.com>
2019-12-03 10:32:46 +0100

    .

--- Unidad_4/src/capicua/Main.java
+++ Unidad_4/src/capicua/Main.java
@@ -3,16 +3,7 @@ package capicua;
 public class Main {
 
 	public static void main(String[] args) {
-		int n=2365;
-		System.out.println(n%10);
-		System.out.println(n/10);
-		int cifra;
-		long aux=n;
-		for (int i = 1; i < 2; i++) {
-			aux/=10;
-		}
-		cifra= (int) (aux%10);
-		System.out.println(cifra);
+		
 		
 	}
 
